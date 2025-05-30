3d0a47c2471663a7036a2e556dce32c636ed25dc
Rudra2808 <rudrapatel6808@gmail.com>
2025-05-30 21:18:46 +0530

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,214 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import { TiLocationArrow } from "react-icons/ti";
+import { useEffect, useRef, useState } from "react";
+
+import Button from "./Button";
+import VideoPreview from "./VideoPreview";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+
+  const [loading, setLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 4;
+  const nextVdRef = useRef(null);
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  useEffect(() => {
+    if (loadedVideos === totalVideos - 1) {
+      setLoading(false);
+    }
+  }, [loadedVideos]);
+
+  const handleMiniVdClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex((prevIndex) => (prevIndex % totalVideos) + 1);
+  };
+
+  useGSAP(
+    () => {
+      if (hasClicked) {
+        gsap.set("#next-video", { visibility: "visible" });
+        gsap.to("#next-video", {
+          transformOrigin: "center center",
+          scale: 1,
+          width: "100%",
+          height: "100%",
+          duration: 1,
+          ease: "power1.inOut",
+          onStart: () => nextVdRef.current.play(),
+        });
+        gsap.from("#current-video", {
+          transformOrigin: "center center",
+          scale: 0,
+          duration: 1.5,
+          ease: "power1.inOut",
+        });
+      }
+    },
+    {
+      dependencies: [currentIndex],
+      revertOnUpdate: true,
+    }
+  );
+
+  useGSAP(() => {
+    gsap.set("#video-frame", {
+      clipPath: "polygon(14% 0, 72% 0, 88% 90%, 0 95%)",
+      borderRadius: "0% 0% 40% 10%",
+    });
+    gsap.from("#video-frame", {
+      clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+      borderRadius: "0% 0% 0% 0%",
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: "#video-frame",
+        start: "center center",
+        end: "bottom center",
+        scrub: true,
+      },
+    });
+  });
+
+  const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
+
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden">
+      {loading && (
+        <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+          {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
+          <div className="three-body">
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+            <div className="three-body__dot"></div>
+          </div>
+        </div>
+      )}
+
+      <div
+        id="video-frame"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"
+      >
+        <div>
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+            <VideoPreview>
+              <div
+                onClick={handleMiniVdClick}
+                className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+              >
+                <video
+                  ref={nextVdRef}
+                  src={getVideoSrc((currentIndex % totalVideos) + 1)}
+                  loop
+                  muted
+                  id="current-video"
+                  className="size-64 origin-center scale-150 object-cover object-center"
+                  onLoadedData={handleVideoLoad}
+                />
+              </div>
+            </VideoPreview>
+          </div>
+
+          <video
+            ref={nextVdRef}
+            src={getVideoSrc(currentIndex)}
+            loop
+            muted
+            id="next-video"
+            className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+          <video
+            src={getVideoSrc(
+              currentIndex === totalVideos - 1 ? 1 : currentIndex
+            )}
+            autoPlay
+            loop
+            muted
+            className="absolute left-0 top-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoad}
+          />
+        </div>
+
+        <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+          G<b>A</b>MING
+        </h1>
+
+        <div className="absolute left-0 top-0 z-40 size-full">
+          <div className="mt-24 px-5 sm:px-10">
+            <h1 className="special-font hero-heading text-blue-100">
+              redefi<b>n</b>e
+            </h1>
+
+            <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+              Enter the Metagame Layer <br /> Unleash the Play Economy
+            </p>
+
+            <Button
+              id="watch-trailer"
+              title="Watch trailer"
+              leftIcon={<TiLocationArrow />}
+              containerClass="bg-yellow-300 flex-center gap-1"
+            />
+          <a href="#" class="group">
+  <div class="bg-gradient-to-r from-[#fc466b] to-[#ff4b2b] inline-flex items-center gap-4 mt-5  py-3 px-12 rounded-full shadow-[0_0_20px_rgba(252,70,107,0.5)] hover:shadow-[0_0_35px_rgba(252,70,107,0.7)] transition-all duration-300 ease-in-out">
+    
+    <div class="bg-white p-1 rounded-full transition-transform duration-300 group-hover:scale-110">
+      <img src="/img/Download-icon.png" alt="Download Icon" class="block size-12" />
+    </div>
+
+    <span class="text-white font-bold text-2xl transition-all duration-300 group-hover:tracking-wider">
+      INSTALL
+    </span>
+
+  </div>
+</a>
+
+{/* <a
+  href="#"
+  className="group mt-10 inline-flex w-80 items-center justify-center gap-4 rounded-full bg-gradient-to-r from-[#fc466b] to-[#ff4b2b] px-8 py-4 text-white text-2xl font-black shadow-[0_0_20px_rgba(252,70,107,0.5)] hover:shadow-[0_0_35px_rgba(252,70,107,0.7)] transition-all duration-300 ease-in-out"
+>
+  
+  <span className="flex items-center justify-center rounded-full bg-white p-4 transition-all duration-300 group-hover:scale-110">
+    <img
+      src="/img/Download-icon.png"
+      alt="Download Icon"
+      className="w-7 h-7 object-contain"
+    />
+  </span>
+
+ 
+  <span className="transition-all duration-300 group-hover:tracking-wider">
+    INSTALL
+  </span>
+</a> */}
+
+
+
+
+
+
+
+
+          </div>
+        </div>
+      </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+        G<b>A</b>MING
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
